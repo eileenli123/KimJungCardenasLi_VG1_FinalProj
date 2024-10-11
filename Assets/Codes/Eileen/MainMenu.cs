6119103e140d68a802fc673e08e4ee04ca1ee9a7
eileenli123 <112430627+eileenli123@users.noreply.github.com>
2024-10-11 02:15:08 -0400

    10/11_eileen:moving camera + lose if off screen, added start and lose screen:

---
+++ Assets/Codes/Eileen/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MainMenu : MonoBehaviour
+{
+    public void startTutorialLevel()
+    {
+        SceneManager.LoadSceneAsync(2); //load scene 1 from build scene setting (tutorial level) 
+    }
+
+    public void goToMainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+}
