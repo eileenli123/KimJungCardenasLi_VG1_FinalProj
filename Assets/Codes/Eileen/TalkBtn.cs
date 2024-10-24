ce502dcbeb5518d945cf3c965fc8961d2a8c0a2a
eileenli123 <112430627+eileenli123@users.noreply.github.com>
2024-10-24 18:04:07 -0500

    10/24 eileen: party obstacle added

---
+++ Assets/Codes/Eileen/TalkBtn.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkBtn : MonoBehaviour
+{
+
+    public GameObject GPA_gem_prefab;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            
+            foreach (ContactPoint2D contact in collision.contacts)
+            {
+                Debug.Log("Contact point normal: " + contact.normal);
+                if (contact.normal.y > 0.9f)
+                {
+                    //create social gem object 
+                    GameObject newGPAgem = Instantiate(GPA_gem_prefab);
+                    Vector3 gemPosition = transform.position;
+                    gemPosition.y += 1f;  // Move the gem up so it doesn't collect right away
+                    newGPAgem.transform.position = gemPosition;
+
+                    Destroy(gameObject);
+
+                }
+            }
+        }
+    }
+
+   
+}
